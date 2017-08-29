b20dc1d015e11cc75b531a53e75e521e904be841
38136 <38136.hexaware@gmail.com>
2017-08-29 17:05:33 +0530

    Add All Files

--- index.js
+++ index.js
@@ -9,4 +9,10 @@ var gr = new GlideRecord(instance, tablename, user, password, 'v1');
 
 gr.query().then(function (result) { //returns promise 
     console.log(result);
-})
+});
+app.get("/",function(req,res){
+    res.send("Localhost Server is  running!!!");
+});
+app.listen(process.env.PORT || 3000, function (message) {
+    console.log("Server is running successfully");
+});
