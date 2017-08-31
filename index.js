65fad19bcb8c0b58441eb9a2c623bccbbdff2d65
38136 <38136.hexaware@gmail.com>
2017-08-31 11:52:35 +0530

    Add All Files

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ var connector = new builder.ChatConnector({
 server.post('/', connector.listen());
 
 var bot = new builder.UniversalBot(connector, function (session) {
-    // session.send("You said: %s", session.message.text);
+    session.send("You said: %s", session.message.text);
     // session.send(JSON.stringify(session.message));
     // do check
     if (session.message.text) {
