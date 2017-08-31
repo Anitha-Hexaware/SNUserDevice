ee100678a288217ca8f110247dc9e7f9d42c74d0
38136 <38136.hexaware@gmail.com>
2017-08-31 09:07:36 +0530

    Add All Files

--- index.js
+++ index.js
@@ -1,65 +1,52 @@
 require('dotenv-extended').load();
-var GlideRecord = require('servicenow-rest').gliderecord;
 var builder = require('botbuilder');
 var restify = require('restify');
+var apiai = require('apiai');
+var APIAII = apiai('854ef36ee9ff4389baf041d8f87e40e0');
+const uuidv1 = require('uuid/v1')();
 var apiairecognizer = require('api-ai-recognizer');
+const unhandledRejection = require("unhandled-rejection");
 var request = require("request");
+var fs = require('fs');
+var serNow = require('servicenow-rest').gliderecord;
 
-//Setup Restify Server
+let rejectionEmitter = unhandledRejection({
+    timeout: 20
+});
+
+rejectionEmitter.on("unhandledRejection", (errro,promise)=>{
+    fs.writeFileSync('./app.json', JSON.stringify(errro),'utf8');
+})
+
+rejectionEmitter.on('rejectionHandled', (error,promise)=>{
+    fs.writeFileSync('./app.json', JSON.stringify(error),'utf8');
+})
+
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 5000, function () {
+server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
-
-// Create chat bot
 var connector = new builder.ChatConnector({
-    appId: '',
-    appPassword: ''
+    appId: 'd60f4a2a-5926-42c5-baa6-db7a8ddcd162',
+    appPassword: '2uiQBbbGGAhkxnGVJDBeb9X'
 });
 
-server.post('/api/messages', connector.listen());
-// let instance = 'dev24552';
-var sN = new GlideRecord({
-    instance:'https://dev24552.service-now.com/',
-    tablename : 'Second_needIt',
-    username : 'admin',
-    password : 'CywRGSYNcFwq',
-    version : 'v1'
- 
-});
-
-//POST Call Handler
-var bot = new builder.UniversalBot(connector);
-var recognizer = new apiairecognizer('50ab8ddd9a594abfbe4cfe1a951dee8d');
-
-bot.recognizer(recognizer);
-
-var intents = new builder.IntentDialog({ recognizers: [recognizer] });
-bot.dialog('/', intents);
-
-intents.matches('add_user', [
-    function (session, args) {
-      console.log("step -1");
-      console.log("Args : "+JSON.stringify(args));
-      var responseString="Hi, Please register the username "
-      session.send(responseString);
-    }
-]);//Welcome Intent Fired
-
-// intents.matches('Add user', [
-//     function (session, args) {
-//         console.log("Add user")
-//         console.log("Args : "+JSON.stringify(args));
-//         var responseString="Sure !"
-//         session.send(responseString);
-//     }
-// ]);
+server.post('/', connector.listen());
 
 
-intents.onDefault(function(session){
-    session.send("Sorry...can you say that again?");
-});
+// var sN = new serviceNow({
+//     instance: 'dev24552',
+//     tablename: 'sys_user',
+//     user: 'admin',
+//     password: 'GcXuKjkVNSZq',
+//     v1: 'v1'
+// });
+// sN.setReturnFields('number', 'short_description');
+// sN.addEncodedQuery('active=true');
+// sN.setLimit(10);
 
-// gr.query().then(function (result) { 
-//     console.log(result);
+// var port = process.env.PORT || 5000;
+// app.set('port', port);
+// app.listen(port, function () {
+//     console.log('Client server listening on port ' + port);
 // });
