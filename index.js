b9460cbe7996be788fcd84029074df95ff4c09ee
38136 <38136.hexaware@gmail.com>
2017-08-31 11:23:00 +0530

    Add All Files

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ var apiairecognizer = require('api-ai-recognizer');
 const unhandledRejection = require("unhandled-rejection");
 var request = require("request");
 var fs = require('fs');
-var serNow = require('servicenow-rest').gliderecord;
+var SerNow = require('servicenow-rest').gliderecord;
 
 let rejectionEmitter = unhandledRejection({
     timeout: 20
 });
 
-rejectionEmitter.on("unhandledRejection", (errro,promise)=>{
-    fs.writeFileSync('./app.json', JSON.stringify(errro),'utf8');
+rejectionEmitter.on("unhandledRejection", (error, promise) => {
+    fs.writeFileSync('./app.json', JSON.stringify(error), 'utf8');
 })
 
-rejectionEmitter.on('rejectionHandled', (error,promise)=>{
-    fs.writeFileSync('./app.json', JSON.stringify(error),'utf8');
+rejectionEmitter.on('rejectionHandled', (error, promise) => {
+    fs.writeFileSync('./app.json', JSON.stringify(error), 'utf8');
 })
 
 var server = restify.createServer();
@@ -37,16 +37,57 @@ var bot = new builder.UniversalBot(connector, function (session) {
     session.send("You said: %s", session.message.text);
     // session.send(JSON.stringify(session.message));
     // do check
-     fs.writeFileSync('./app.json', JSON.stringify(session.message),'utf8');
-     if(session.message.text){
-         var req = APIAII.textRequest(session.message.text,{
-             sessionId : uuidv1
-         });
-         request.on('response', function(response){
+    if (session.message.text) {
+        var req = APIAII.textRequest(session.message.text, {
+            sessionId: uuidv1
+        });
+
+
+        request.on('response', function (response) {
             let result = response.result;
             session.send(JSON.stringify(result));
-         });
-     }
+            if (result.metadata.intentName == "Default Welcome Intent") {
+                session.send("Hi welcome !! \n\n How may I help you");
+            } else if (result.metadata.intentName == "User registration") {
+                let email = result.parameters["email"];
+                let fName = result.parameters["given-name"];
+                let lName = result.parameters["last-name"];
+                let empId = result.parameters["number"];
+                // let userPassword= result.parameters["password"];
+                // let userNameintent= result.parameters["username"];
+                if (email != "" && fName !== "" && lName !== "" && empId !== "") {
+                    var userdetails = {
+                        email: email,
+                        uFirstName: fName,
+                        uLastName: lName,
+                        uEmpId: empId
+                    };
+                    // var sN = new SerNow({
+                    //     instance: 'dev24552',
+                    //     tablename: 'sys_user',
+                    //     user: 'admin',
+                    //     password: 'GcXuKjkVNSZq',
+                    //     v1: 'v1'
+                    // });
+
+                    var sN = new SerNow('dev24552', 'sys_user', 'admin', 'GcXuKjkVNSZq', 'v1');
+
+                    sN.insert(obj).then(function (response) {
+                        session.send('Thank for your patience , Make sure you can login to update the details.')
+                    });
+
+                } else {
+                    session.send(result.fulfillment.speech);
+                }
+            } else if (result.metadata.intentName == "Default Fallback Intent") {
+                session.send(result.fulfillment.speech);
+            }
+        });
+        request.on('error', function (error) {
+            //  console.log(error);
+        });
+        request.end();
+    }
 
 
 });
